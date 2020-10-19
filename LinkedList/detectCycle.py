e2fcc7ba73c3c57d3f06d842a81c22bb5950be84
Amy Deng <kexindeng2000@gmail.com>
2020-10-19 14:58:35 +0700

    completed detectCycle

---
+++ LinkedList/detectCycle.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def detectCycle(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+        if not (fast and fast.next):
+            return null
+        fast = head
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+        return fast
