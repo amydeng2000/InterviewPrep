f2b07644b70d3c3485abcfc1a0492be05c5db186
Amy Deng <kexindeng2000@gmail.com>
2020-10-28 09:46:40 -0700

    coin change recursive

--- DP/coinChange.py
+++ DP/coinChange.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class SolutionIterative(object):
     def coinChange(self, coins, amount):
         """
         :type coins: List[int]
@@ -13,4 +13,27 @@ class Solution(object):
             for i in range(coin, amount+1):
                 mem[i] = min(mem[i], 1+mem[i-coin])
         if mem[amount] == float('inf'): return -1
+        return mem[amount]
+
+
+class SolutionRecursive(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        mem = [float('inf')]*(amount+1)
+        return self.helper(coins, amount, mem)
+        
+    def helper(self, coins, amount, mem):
+        if amount == 0: return 0
+        if amount < 0: return -1
+        if mem[amount] != float('inf'): return mem[amount]
+        minimum = float('inf')
+        for coin in coins:
+            flag = self.helper(coins, amount-coin, mem)
+            if flag >= 0 and flag < minimum:
+                minimum = 1+flag
+        mem[amount] = -1 if minimum == float('inf') else minimum     
         return mem[amount]
