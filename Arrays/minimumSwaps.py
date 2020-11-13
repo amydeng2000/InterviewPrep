77589b200ef4482c35e185a6bb8ba770f51e222d
Amy Deng <kexindeng2000@gmail.com>
2020-11-12 21:08:45 -0800

    changing min swaps

--- Arrays/minimumSwaps.py
+++ Arrays/minimumSwaps.py
@@ -2,9 +2,11 @@ def minimumBribes(q):
     swaps = 0
     for i in range(len(q)):
         if q[i] > i + 3:
-            return "Too chaotic"
-        elif q[i] > i+1:
-            swaps += q[i] - (i+1)
-    return swaps
-
-print(minimumBribes([2,1,5,3,4]))
+            print("Too chaotic")
+            return
+        start = max(0, q[i]-2)
+        for j in range(start, i):
+            if q[j] > q[i]:
+                swaps += 1
+    print(swaps)
+    return 
