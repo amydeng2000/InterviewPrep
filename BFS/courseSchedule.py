4d82a0bdb10db44d439c8212034b02fd998d6073
Amy Deng <kexindeng2000@gmail.com>
2020-11-16 13:08:09 -0800

    populate next pointer

---
+++ BFS/courseSchedule.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    # prerequisites = [[take_after, take_before]]
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        print(0 in [0, 1])
+        for i in range(numCourses):
+            mem = []
+            if self.dfs(i, prerequisites, mem, True) == False: 
+                return False
+        return True
+    
+    def dfs(self, course, prerequisites, mem, flag):
+        mem.append(course)
+        for p in prerequisites:
+            if not flag: return False
+            if p[0] == course:
+                print(p[1])
+                print(mem)
+                if p[1] in mem: 
+                    flag = False
+                    return False
+                self.dfs(p[1], prerequisites, mem, flag)
+        return flag
+
+s = Solution()
+prereq = [[0,1], [1,0]]
+print(s.canFinish(2, prereq))
