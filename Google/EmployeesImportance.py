c771dc9b312af0337ae8c63b0a14dc5da28cb75d
Amy Deng <kexindeng2000@gmail.com>
2020-12-02 23:22:24 -0800

    employee importance

---
+++ Google/EmployeesImportance.py
@@ -0,0 +1,31 @@
+"""
+# Definition for Employee.
+class Employee(object):
+    def __init__(self, id, importance, subordinates):
+    	#################
+        :type id: int
+        :type importance: int
+        :type subordinates: List[int]
+        #################
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: List[Employee]
+        :type id: int
+        :rtype: int
+        """
+        d = {}
+        for e in employees:
+            d[e.id] = e
+        def dfs(id):
+            total = d[id].importance
+            for sub in d[id].subordinates:
+                total += dfs(sub)
+            return total
+        return dfs(id)
+        
