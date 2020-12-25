a0a62e8f4d307190b8fc82159a3281a453bf4845
Amy Deng <kexindeng2000@gmail.com>
2020-12-24 21:46:14 -0600

    dp

---
+++ DP/DP.py
@@ -0,0 +1,13 @@
+"""
+Process of solving a DP problem:
+- can the problem be broken down into smaller subproblems?
+- Identify changing parameters & write out recursive cases
+- Base cases (out of bound, reach goal)
+- Memoization: store before return, check as a base case 
+"""
+
+# Common Methods
+"""
+- One pass: keep track of minimum and maxDiff, update values as you go through one for loop 
+- 
+"""
